d461ee0bd558276d93e3c0e5c59ac04ef96c1f03
Anthony Carrillo <acarrillorj@gmail.com>
2020-10-09 23:17:44 -0500

    new branch

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/youtube-app'));
+app.use(express.static(__dirname + '/dist/youtubeApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/youtube-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/youtubeApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
